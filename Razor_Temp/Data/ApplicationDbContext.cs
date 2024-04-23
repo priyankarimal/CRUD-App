2b7f905acf123971e15c00b55f2238581007872e
Priyanka <priyankarimal87@gmail.com>
2024-04-23 15:10:42 +0545

    created applicationdbcontext class

--- Razor_Temp/Data/ApplicationDbContext.cs
+++ Razor_Temp/Data/ApplicationDbContext.cs
@@ -17,6 +17,7 @@ namespace Razor_Temp.Data
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+
                 );
         }
 
